5f4ad32a846477f12444088e5a456e52a2df5363
javier.vera <javier.vera@plainconcepts.com>
2024-03-13 17:18:14 +0100

    updated data agumentation

--- IntrA_transformers/data_loader.py
+++ IntrA_transformers/data_loader.py
@@ -8,10 +8,11 @@ import math
 import random
 
 class IntrADataGenerator(Dataset):
-    def __init__(self, data_folder_path, file_names) -> None:
+    def __init__(self, data_folder_path, file_names, data_aug=True) -> None:
         super().__init__()
         self.data_folder_path = data_folder_path
         self.file_names = file_names
+        self.data_aug = data_aug
 
     def __len__(self):
         return len(self.file_names)
@@ -23,22 +24,32 @@ class IntrADataGenerator(Dataset):
         mesh = trimesh.load_mesh(os.path.join(self.data_folder_path, "obj", self.file_names[idx]))
         center = mesh.centroid
         faces = mesh.faces
-        angle1 = random.uniform(0, math.pi)
-        rot_mat1 = trimesh.transformations.rotation_matrix(angle1, [1,0,0], center)
+        if self.data_aug:
+            angle1 = random.uniform(0, math.pi)
+            rot_mat1 = trimesh.transformations.rotation_matrix(angle1, [1,0,0], center)
+            
+            angle2 = random.uniform(0, math.pi)
+            rot_mat2 = trimesh.transformations.rotation_matrix(angle2, [0,1,0], center)
+            
+            angle3 = random.uniform(0, math.pi)
+            rot_mat3 = trimesh.transformations.rotation_matrix(angle3, [0,0,1], center)
+            mesh.apply_transform(rot_mat1@rot_mat2@rot_mat3)
         
-        angle2 = random.uniform(0, math.pi)
-        rot_mat2 = trimesh.transformations.rotation_matrix(angle2, [0,1,0], center)
-        
-        angle3 = random.uniform(0, math.pi)
-        rot_mat3 = trimesh.transformations.rotation_matrix(angle3, [0,0,1], center)
-        mesh.apply_transform(rot_mat1@rot_mat2@rot_mat3)
 
         tras_mat = trimesh.transformations.translation_matrix(-mesh.centroid)
         mesh.apply_transform(tras_mat)
 
+        if self.data_aug:
+            if random.uniform(0,1)>0.1:
+                scaler = np.array([random.uniform(0.75,1.15), random.uniform(0.75,1.15), random.uniform(0.75,1.15)]) 
+                mesh.vertices = mesh.vertices*scaler
+        
+        norm_coords = np.max(np.abs(mesh.vertices))
+
+
         assert len(mesh.vertices) == data_array.shape[0]
         data_array[data_array[:,-1] == 2, -1] = 1
-        feats = np.concatenate([np.array(mesh.vertices)/np.max(np.abs(mesh.vertices),axis=0), np.array(mesh.vertex_normals)], axis = 1)
+        feats = np.concatenate([np.array(mesh.vertices)/norm_coords, np.array(mesh.vertex_normals)], axis = 1)
 
         return [feats, data_array[:,[-1]],torch.from_numpy(np.array(faces))]
 
@@ -67,106 +78,10 @@ def get_generator_set(config: TrainConfigMaker):
                             num_workers=8)
 
     val_loader =  DataLoader(IntrADataGenerator(config.dataset_config.input_data_dir_path,
-                                                  config.dataset_config.val_data_split),
+                                                  config.dataset_config.val_data_split, data_aug=False),
                             shuffle=True,
                             batch_size=config.dataset_config.train_batch_size,
                             collate_fn=collate_fn,
                             num_workers=2)
 
     return [train_loader, val_loader]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for data loader testing and demo
-if __name__ == "__main__":
-    import pandas as pd
-    from gen_utils import (
-        rodrigues_rotation_matrix,
-        get_rotation_angles_from_rot_mat,
-    )
-    from time import sleep
-
-    df = pd.read_csv("output\\movements.csv")
-
-    data_generator = DentalMovModelGenerator(
-        ".",
-        df,
-        augment=True,
-    )
-    LABEL = 16
-    for batch in data_generator:
-        gt_angles = batch["gt_movements"][LABEL, 0:3].reshape(1, 3)
-        rot_mat_rodrigues = rodrigues_rotation_matrix(torch.Tensor(gt_angles))
-        # rot_mat_rodrigues_roma = rotvec_to_rotmat(torch.Tensor(gt_angles[0]))
-        # print(
-        #     f"error in rodrigues owncode vs roma: \
-        #     {np.mean(np.abs(rot_mat_rodrigues.numpy() - rot_mat_rodrigues_roma.numpy()))}"
-        # )
-        angles = get_rotation_angles_from_rot_mat(rot_mat_rodrigues.numpy()[0:3, 0:3])
-        print(f"angles_gt: {gt_angles}")
-        print(f"angles from rodrigues: {angles}")
-
-        rot_mat_x = trimesh.transformations.rotation_matrix(gt_angles[0, 0], [1, 0, 0])
-        rot_mat_y = trimesh.transformations.rotation_matrix(gt_angles[0, 1], [0, 1, 0])
-        rot_mat_z = trimesh.transformations.rotation_matrix(gt_angles[0, 2], [0, 0, 1])
-
-        rot_mat = np.dot(rot_mat_z, np.dot(rot_mat_y, rot_mat_x))
-
-        gs_rot_mat = get_rotation_matrix_from_vecs(
-            torch.Tensor(rot_mat[0, 0:3]), torch.Tensor(rot_mat[1, 0:3])
-        )
-        angles_gs = get_rotation_angles_from_rot_mat(gs_rot_mat.numpy()[0:3, 0:3])
-        print(f"angles from gramsmitch: {angles_gs}")
-
-        pre_coord = batch["feat"][batch["gt_labels"] == LABEL, 0:3]
-        C = batch["feat"][batch["gt_labels"] == LABEL, -3::]
-
-        centered_pre_coord = pre_coord - C
-
-        post_coords_applying_gt = (
-            centered_pre_coord.dot(rot_mat[0:3, 0:3].T)
-            + batch["gt_movements"][LABEL, 3:6]
-            + C
-        )
-        post_coords_from_rodrigues = (
-            centered_pre_coord.dot(rot_mat_rodrigues[0:3, 0:3].T)
-            + batch["gt_movements"][LABEL, 3:6]
-            + C
-        )
-        post_coords_from_gs = (
-            centered_pre_coord.dot(gs_rot_mat[0:3, 0:3].T)
-            + batch["gt_movements"][LABEL, 3:6]
-            + C
-        )
-
-        post_coords_in_gt = batch["gt_points"][batch["gt_labels"] == LABEL]
-
-        error_using_gt_angles = np.mean(
-            np.abs(post_coords_applying_gt - post_coords_in_gt)
-        )
-        error_using_rodrigues = np.mean(
-            np.abs(post_coords_from_rodrigues - post_coords_in_gt)
-        )
-        error_using_gs = np.mean(np.abs(post_coords_from_gs - post_coords_in_gt))
-
-        print(f"error_using_gt_angles: {error_using_gt_angles}")
-        print(f"error_using_rodrigues: {error_using_rodrigues}")
-        print(f"error_using_gs: {error_using_gs}")
-        sleep(3)
